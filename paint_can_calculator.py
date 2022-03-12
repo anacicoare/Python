99649a3299e3875e1dcda3ed3000f72868f7a286
Ana Cicoare <anacicoare03@gmail.com>
2022-03-12 12:25:05 +0200

    Added coffe machine project

--- paint_can_calculator.py
+++ paint_can_calculator.py
@@ -1,12 +1,12 @@
-#number of cans to spray paint a wall, knowing a can covers up 5 m^2, and giving the height and the lenght of the wall as an input
+#number of cans to spray paint a wall, knowing a can covers up 5, and giving the height and the lenght of the wall as an input
 import math
+
 def paint_calc(height, width, cover):
     total_cans = math.ceil(height * width / cover)
-    print(f"You need {total_cans} cans of paint")
-
-
+    print(f"You need {total_cans} cans of paint.")
 
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
+
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
